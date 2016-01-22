d54d69bb9ca7dd5122283bd10c0b7981f44dba1a
liuxu <liuxu-0703@163.com>
2016-01-22 13:28:41 +0800

    add Animation package for view animation util class

---
+++ appframe/src/main/java/lx/af/utils/Animation/AnimationEndListener.java
@@ -0,0 +1,16 @@
+package lx.af.utils.Animation;
+
+import android.view.animation.Animation;
+
+/**
+ * author: lx
+ * date: 16-1-21
+ */
+public abstract class AnimationEndListener implements Animation.AnimationListener {
+    @Override
+    public void onAnimationStart(Animation animation) {
+    }
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+    }
+}
